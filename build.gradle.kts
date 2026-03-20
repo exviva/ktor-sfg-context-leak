da5cdc5dd8f834779f0d01158e78294d56cc1df2
Olek Janiszewski <olek.janiszewski@airbnb.com>
2026-03-20 07:49:16 +0100

    Simplify further
    Co-authored-by: Olek Janiszewski <olek.janiszewski@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,6 @@ kotlin {
 dependencies {
     implementation(libs.kotlinx.coroutines.core)
     implementation(libs.kotlinx.coroutines.slf4j)
-    implementation(libs.ktor.server.call.logging)
     implementation(libs.ktor.server.core)
     implementation(libs.ktor.server.config.yaml)
     implementation(libs.ktor.server.netty)
--- src/main/kotlin/Routing.kt
+++ src/main/kotlin/Routing.kt
@@ -1,17 +1,39 @@
 package com.example
 
 import io.ktor.server.application.*
-import io.ktor.server.plugins.calllogging.*
-import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
-import org.slf4j.event.*
+import io.ktor.util.AttributeKey
+import io.opentelemetry.api.trace.Span
+import io.opentelemetry.context.Context
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import java.lang.Thread.sleep
 
 fun Application.configureRouting() {
     routing {
         get("/") {
             call.application.log.info("hello from route")
+            val callTraceId =
+                call
+                    .attributes
+                    .allKeys
+                    .find { it.name == "OpenTelemetry" }
+                    ?.let {
+                        @Suppress("UNCHECKED_CAST")
+                        it as AttributeKey<Context>
+                    }
+                    ?.let(call.attributes::get)
+                    ?.let(Span::fromContext)
+                    ?.spanContext
+                    ?.traceId
+                    ?: "NULL"
+
+            call.application.log.info("route: otel context in call attributes: $callTraceId")
             call.respondText("Hello World!")
+            withContext(Dispatchers.IO) {
+                sleep(10)
+            }
         }
     }
 }
