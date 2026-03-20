773f100de707e5323f60b23698e9916e1c11dcd5
Olek Janiszewski <olek.janiszewski@airbnb.com>
2026-03-20 18:16:42 +0100

    Remove Logback
    Co-authored-by: Olek Janiszewski <olek.janiszewski@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,11 +25,9 @@ dependencies {
     implementation(libs.ktor.server.core)
     implementation(libs.ktor.server.config.yaml)
     implementation(libs.ktor.server.netty)
-    implementation(libs.logback.classic)
     implementation(libs.opentelemetry.exporter.otlp)
     implementation(libs.opentelemetry.extension.kotlin)
     implementation(libs.opentelemetry.instrumentation.ktor)
-    implementation(libs.opentelemetry.instrumentation.logback)
     implementation(libs.opentelemetry.sdk)
     implementation(libs.opentelemetry.sdk.logs)
 
